e9730a503b0ef22d046d468e90de5e7449de758e
Mohamed KAMLI <mohamed.kamli@epitech.eu>
2025-02-24 23:57:14 +0100

    [ADD] add TypeScript preset to Babel configuration and update package.json dependencies

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: [
     '@babel/preset-env',
     '@babel/preset-react', // Ajoutez cette ligne pour inclure @babel/preset-react
+    '@babel/preset-typescript', // Transpile le code TypeScript en JavaScript standard
   ],
   plugins: [
     // Ajoutez d'autres plugins ici si nécessaire
