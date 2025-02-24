5f5417b55dcd89c5d20ca3873a6f9b81616e87d0
Mohamed KAMLI <mohamed.kamli@epitech.eu>
2025-02-24 11:12:31 +0100

    [ADD] implement async token retrieval in axios interceptor for Trello API requests

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -1,52 +1,62 @@
-import axios, { get } from "axios";
+import axios from "axios";
 import dotenv from "dotenv";
+import { getToken } from './trello/auth'; // Importer la fonction getToken
+
 dotenv.config();
-import { getToken } from './trello/auth'; 
 
 const TRELLO_API_KEY = process.env.TRELLO_API_KEY;
 
-
-// Création d'une instance d'axios avec une configuration de base
-const httpClient =  axios.create({
+// Création d'une instance unique d'axios avec une configuration de base
+const httpClient = axios.create({
   baseURL: "https://api.trello.com/1",
   params: {
     key: TRELLO_API_KEY,
   },
 });
 
+// Token d'authentification
+let authToken: string | null = null;
 
-//intercepteur axios pour l'entête de la requête --> récupère ici le token de manière asynchrone
+const updateToken = async () => {
+  authToken = await getToken();
+};
+
+// explication : Comme l'intercepteur ne peut pas être asynchrone à cause du typage strict en ts de axios, 
+// on utilise une fonction(updateToken) pour mettre à jour le token et on l'appelle dans l'intercepteur
+
+// Intercepteur pour les requêtes
 httpClient.interceptors.request.use(
-    config => {
-      const token = getToken();
-      if (!config.headers) {
-        config.headers = {};
-      }
-      config.headers.Authorization= `Bearer ${token}`;
-      console.log('Requête envoyée:', config);
-      return config;
-    },
-    error => {
-      return Promise.reject(error);
+  (config) => {
+    if (!config.headers) {
+      config.headers = {};
     }
-  );
-  
-// intercepteur axios pour la réponse et les erreurs
+    if (authToken) {
+      config.headers.Authorization = `Bearer ${authToken}`;
+    }
+    console.log("Requête envoyée:", config);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Intercepteur pour les réponses
 httpClient.interceptors.response.use(
-    response => {
-      console.log('Réponse reçue:', response);
-      return response;
-    },
-    error => {
-      if (error.response) {
-        console.error('Erreur de réponse:', error.response);
-      } else if (error.request) {
-        console.error('Erreur de requête:', error.request);
-      } else {
-        console.error('Erreur:', error.message);
-      }
-      return Promise.reject(error);
+  response => {
+    console.log('Réponse reçue:', response);
+    return response;
+  },
+  error => {
+    if (error.response) {
+      console.error('Erreur de réponse:', error.response);
+    } else if (error.request) {
+      console.error('Erreur de requête:', error.request);
+    } else {
+      console.error('Erreur:', error.message);
     }
-  );
+    return Promise.reject(error);
+  }
+);
 
 export default httpClient;
