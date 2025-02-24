b4dfeb08527c7c4460e478c201ff29a47ea24e17
Mohamed KAMLI <mohamed.kamli@epitech.eu>
2025-02-24 23:52:23 +0100

    [ADD] configure Babel with React preset and update app.json for iOS bundle identifier

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react', // Ajoutez cette ligne pour inclure @babel/preset-react
+  ],
+  plugins: [
+    // Ajoutez d'autres plugins ici si nécessaire
+  ],
+};
