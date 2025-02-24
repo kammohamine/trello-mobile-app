1afde2be4eace308275776add56d66baaea3f342
Mohamed KAMLI <mohamed.kamli@epitech.eu>
2025-02-24 13:00:00 +0100

    [ADD] implement getIdMembers function to retrieve assigned member IDs for Trello cards

--- src/api/trello/assign.ts
+++ src/api/trello/assign.ts
@@ -1,4 +1,20 @@
 import httpClient from "../httpClient";
+import { getToken } from './auth';
+
+//Avant d'assigner, il faut get l'ID
+export const getIdMembers = async (id: string) => {
+  try {
+    const response = await httpClient.get(`/cards/${id}/idMembers`, {
+        params: {
+          key: process.env.TRELLO_API_KEY,
+          token: await getToken(),
+        },
+      });    return response.data;
+} catch (error) {
+    console.error('Erreur lors de la récupération des membres assignés à la carte:', error);
+    throw error;
+  }
+}
 
 // Assigner des personnes à une carte
 export const assignPersons = async (id: string, idMembers: string[]) => {
@@ -11,4 +27,4 @@ export const assignPersons = async (id: string, idMembers: string[]) => {
     console.error('Erreur lors de l\'assignation des personnes à la carte:', error);
     throw error;
   }
-};
+}
